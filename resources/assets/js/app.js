a3c92313a7c37d5f4c0fa077e90ffabe1f326593
Wim Luiten <wim@tjuna.com>
2018-01-27 11:45:20 +0100

    Vue’s and broadcasts

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,18 +5,39 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+// resources/assets/js/app.js
+
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
 
 const app = new Vue({
-    el: '#app'
+  el: '#app',
+
+  data: {
+    messages: []
+  },
+
+  created() {
+    this.fetchMessages();
+  },
+
+  methods: {
+    fetchMessages() {
+      axios.get('/messages').then(response => {
+        this.messages = response.data;
+    });
+    },
+
+    addMessage(message) {
+      this.messages.push(message);
+
+      axios.post('/messages', message).then(response => {
+        console.log(response.data);
+    });
+    }
+  }
 });
